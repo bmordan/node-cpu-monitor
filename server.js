9f51dabb5874a7c3917a44811a1bf352263be08b
Bernard Mordan <bernardmordan@gmail.com>
2014-10-28 19:59:46 +0000

    awesome os-monitor working, and being sent to browser

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var expressLayouts = require('express-ejs-layouts');
+var monitor = require('os-monitor')
+var stream = monitor.start({ delay: 60, stream: true });
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -14,3 +16,12 @@ app.get('/', function(req, res){
 http.listen(2000, function(){
   console.log("CPU METER server running on :2000")
 })
+
+io.on('connection', function(socket) {
+  stream.on('monitor', function(event) {
+    var loadavg = Math.floor(event.loadavg[0]*100);
+    socket.emit('useage', { useage: loadavg } );
+  })
+})
+
+
